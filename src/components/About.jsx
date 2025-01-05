6752e180525c7adedc9d3e44515254e297869a1a
DengmengLiu <Dengmeng.liu@student.uts.edu.au>
2025-01-05 21:31:48 +1100

    hero section updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React from 'react';
+import Tilt from 'react-parallax-tilt';
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <motion.div>
+        <p className={styles.sectionTitle}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p variants={fadeIn("", "", 0.1, 1)} 
+      className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
+        I'm a skilled Unity developer with experience in creating 
+        immersive 3D experiences and games. I specialize in C# 
+        programming, game mechanics implementation, and optimizing 
+        performance. My strong foundation in computer graphics and 
+        physics enables me to develop engaging interactive experiences 
+        while maintaining efficient resource management.
+      </motion.p>
+    </>
   )
 }
 
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
-const Computers = () => {
+import CanvasLoader from "../Loader";
+
+const Computers = ({ isMoblie }) => {
+    const computer = useGLTF("/desktop_pc/scene.gltf");
+
+    return (
+        <mesh>
+            <hemisphereLight intensity={0.15} groundColor="black" />         
+            <pointLight intensity={1} />
+            <spotLight
+                position={[-20, 50, 10]}
+                angle={0.12}
+                penumbra={1}
+                intensity={5000}
+                castShadow
+                shadow-mapSize={1024}
+            />
+            <primitive
+                object={computer.scene}
+                scale={isMoblie ? 0.65 : 0.75}
+                position={isMoblie ? [0, -3, -2.2]:[0, -3.25, -1.5]}
+                rotation={[-0.01, -0.2, -0.1]}
+            />
+        </mesh>
+    );
+};
+
+const ComputersCanvas = () => {
+  const [isMoblie, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    //add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+    
+    //set the initial value of the "isMobile" state variable
+    setIsMobile(mediaQuery.matches);
+
+    //define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => { setIsMobile(event.matches);};
+
+    //add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    //remove the listener when the component is unmounted
+    return () =>{
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
-    <div>Computers</div>
-  )
-}
+      <Canvas
+        frameloop='demand'
+        shadows
+        dpr={[1, 2]}
+        camera={{ position: [20, 3, 5], fov: 25 }}
+        gl={{ preserveDrawingBuffer: true }}
+      >
+        <Suspense fallback={<CanvasLoader />}>
+          <OrbitControls
+              enableZoom={false}
+              maxPolarAngle={Math.PI / 2}
+              minPolarAngle={Math.PI / 2}
+          />
+          <Computers isMoblie={isMoblie} />
+        </Suspense>
+        <Preload all />
+      </Canvas>
+  );
+};
 
-export default Computers
+export default ComputersCanvas;
