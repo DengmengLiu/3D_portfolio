1085cd02414ae1b937e08b880c91c3d99d85a21c
DengmengLiu <Dengmeng.liu@student.uts.edu.au>
2025-01-07 16:11:27 +1100

    About section updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,17 +14,5 @@ const About = () => {
         <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
 
-      <motion.p variants={fadeIn("", "", 0.1, 1)} 
-      className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
-        I'm a skilled Unity developer with experience in creating 
-        immersive 3D experiences and games. I specialize in C# 
-        programming, game mechanics implementation, and optimizing 
-        performance. My strong foundation in computer graphics and 
-        physics enables me to develop engaging interactive experiences 
-        while maintaining efficient resource management.
-      </motion.p>
-    </>
-  )
-}
 
 export default About
